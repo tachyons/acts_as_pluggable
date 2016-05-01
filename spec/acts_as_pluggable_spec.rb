98dcff36602af6d82152c44c6ee70427a19b7959
Aboobacker MK <aboobackervyd@gmail.com>
2016-05-01 10:44:34 +0530

    test for unknwon plugin

--- spec/acts_as_pluggable_spec.rb
+++ spec/acts_as_pluggable_spec.rb
@@ -8,8 +8,13 @@ describe ActsAsPluggable do
   it 'does something useful' do
     expect(true).to eq(true)
   end
+
   it "should raise exception for duplicate plugins" do
     ActsAsPluggable::Plugin.register :id, {}
     expect { ActsAsPluggable::Plugin.register :id, {} }.to raise_error(ActsAsPluggable::DuplicatePlugin)
+  end
+
+  it "should raise exception for unknown plugins" do
+    expect { ActsAsPluggable::Plugin.find :unknown }.to raise_error(ActsAsPluggable::PluginNotFound)
   end
 end
